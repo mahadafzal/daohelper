321201678f145a52e137ea4461489988e599c1e0
mahadafzal <mahad.bhatti@richmond.edu>
2022-01-29 14:25:49 -0500

    User Registration using Postman endpoints

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const {check, validationResult} = require('express-validator');
+
+const User = require('../../Models/User');
 
 // @route   POST api/users
 // @desc    Register user
@@ -12,13 +16,50 @@ router.post('/',
     check('password', 'Please enter a password with at least 5 chars').isLength({min: 5})
 ],
 
-(req, res) => {
+async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()});
     }
-    console.log(req.body);
-    res.send('User route');
+
+    const {name, email, password} = req.body;
+
+    try {
+
+        let user = await User.findOne({email});
+
+        if(user) {
+            res.status(400).json({errors: [{msg: 'User already exists'}]});
+        }
+
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        });
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+
+        // Encrypt password
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        // Return jsonwebtoken
+        res.send('User registered');
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+
 });
 
 module.exports = router;
